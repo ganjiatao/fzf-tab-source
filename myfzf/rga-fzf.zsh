174f3c06c52d2fcb4f10c64d3d44a7238ceb08fe
LINGGUANG <17671304431@163.com>
2023-07-26 09:26:05 +0800

    完善rga-fzf

--- myfzf/rga-fzf.zsh
+++ myfzf/rga-fzf.zsh
@@ -11,12 +11,12 @@ else
         --bind "start:reload($RGA_PREFIX {q})+unbind(ctrl-r)" \
         --bind "change:reload:sleep 0.1; $RGA_PREFIX {q} || true" \
         --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rga-fzf-r; cat /tmp/rga-fzf-f)" \
-        --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. ripgrep> )+disable-search+reload($RGA_PREFIX {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rga-fzf-f; cat /tmp/rga-fzf-r)" \
+        --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. ripgrep-all> )+disable-search+reload($RGA_PREFIX {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rga-fzf-f; cat /tmp/rga-fzf-r)" \
         --color "hl:-1:underline,hl+:-1:underline:reverse" \
         --prompt '1. ripgrep-all> ' \
         --delimiter : \
         --header '╱ CTRL-R (ripgrep-all mode) ╱ CTRL-F (fzf mode) ╱' \
-        --preview 'bat --color=always {1} --highlight-line {2}' \
+        --preview 'rga "" {1} | bat --color=always  --highlight-line {2}' \
         --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
         --bind 'enter:become(nvim {1} +{2})'
 fi
