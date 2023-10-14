097bd9110cd0c44802a79fb4045b1e38516f220f
LINGGUANG <17671304431@163.com>
2023-10-15 00:03:26 +0800

    修改z.lua配置

--- myfzf/fzf-config.zsh
+++ myfzf/fzf-config.zsh
@@ -1,7 +1,11 @@
 # #! /usr/bin/zsh 
 export PreViewFilePath="${0:A:h}/file_preview.zsh"
 export RUNEWIDTH_EASTASIAN=0
-export FZF_DEFAULT_OPTS="--preview 'zsh $PreViewFilePath {}' --height 30 --layout=reverse --history=$HOME/.fzfhistory"
+# export FZF_DEFAULT_OPTS="--preview 'zsh $PreViewFilePath {}' --height 30 --layout=reverse --history=$HOME/.fzfhistory"
+
+export FZF_DEFAULT_OPTS=" --layout=reverse --history=$HOME/.fzfhistory"
+
+
 
 if command -v fd &>/dev/null; then
 	export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.node_modules,bulid,dist,vender,mnt} --type f --type l" 
@@ -9,19 +13,44 @@ else
 	export FZF_DEFAULT_COMMAND='find . \( -path "*/.git" -o -path "*/.idea" -o -path "*/.vscode" -o -path "*/node_modules" -o -path "*/build" -o -path "*/dist" -o -path "*/vendor" -o -path "*/mnt" \) -prune -o -type f -o -type l -print'
 fi
 
+# 测试
+
+#FZF-TAB右侧窗口配置
+# local extract="
+# # trim input
+# local in=\${\${\"\$(<{f})\"%\$'\0'*}#*\$'\0'}
+# # get ctxt for current completion
+# local -A ctxt=(\"\${(@ps:\2:)CTXT}\")
+# # real path
+# local realpath=\${ctxt[IPREFIX]}\${ctxt[hpre]}\$in
+# realpath=\${(Qe)~realpath}
+# "
+# zstyle ':fzf-tab:complete:kill:argument-rest' extra-opts --preview=$extract'ps --pid=$in[(w)1] -o cmd --no-headers -w -w' --preview-window=down:3:wrap
+# zstyle ':fzf-tab:complete:(cd|ls|exa|bat|cat|nano|vi|vim):*' extra-opts --preview=$extract'ls -1 -A --color=always $realpath'
+# zstyle ':fzf-tab:complete:z:*' fzf-preview 'z -I $word'
+
+# aslas (date; ps -ef) |
+#   fzf --bind='ctrl-r:reload(date; ps -ef)' \
+#       --header=$'Press CTRL-R to reload\n\n' --header-lines=2 \
+#       --preview='echo {}' --preview-window=down,3,wrap \
+#       --layout=reverse --height=80% | awk '{print $2}' | xargs kill -9
 
 # my
 zstyle ':completion:complete:*:options' sort false
 zstyle ':fzf-tab:complete:cd:*' query-string input
+# zstyle ':fzf-tab:complete:z:*' command 'z -I -t . '
+# zstyle ':complete:z:*'  query-string input
 zstyle ':completion:*:descriptions' format "[%d]"
-zstyle ':fzf-tab:*' group-colors $'\033[15m' $'\033[14m' $'\033[33m'$'\033[35m' $'\033[15m' $'\033[14m' $'\033[33m'$'\033[35m'
+# zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# zstyle ':fzf-tab:*' group-colors $'\033[15m' $'\033[14m' $'\033[33m'$'\033[35m' $'\033[15m' $'\033[14m' $'\033[33m'$'\033[35m'
 zstyle ':fzf-tab:*' prefix ''
 
 zstyle ':fzf-tab:complete:*:*' fzf-preview 'zsh $PreViewFilePath ${(Q)realpath}'
-# zstyle ':fzf-tab:complete:*:*' fzf-preview 'zsh /opt/fzf-tab/myfzf/file_preview.zsh ${(Q)realpath}'
 zstyle ':fzf-tab:complete:*:*' fzf-flags --height=30
 zstyle ':fzf-tab:complete:yay:*' fzf-preview 'yay -Qi $word || yay -Si $word'
 zstyle ':fzf-tab:complete:pacman:*' fzf-preview 'pacman -Qi $word || pacman -Si $word'
+
+
 # man
 zstyle ':fzf-tab:complete:(\\|)run-help:*' fzf-preview 'run-help $word'
 zstyle ':fzf-tab:complete:(\\|*/|)man:*' fzf-preview 'man $word'
